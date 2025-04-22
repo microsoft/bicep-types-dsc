d0fd8e66cbd0a3be8642b9d22ccd768c3d56b89d
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-04-21 20:43:13 -0700

    Simplify

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,13 @@ import { version } from "../package.json";
 // TODO: import { TypeFactory } from "bicep-types";
 import axios from "axios";
 import addFormats from "ajv-formats";
-import Ajv2020, { type ValidateFunction } from "ajv/dist/2020";
+import Ajv2020 from "ajv/dist/2020";
 
 type UnknownJson = Record<string, unknown>;
 
 async function loadSchema(uri: string): Promise<UnknownJson> {
   const response = await axios.get(uri);
   return response.data as UnknownJson;
-}
-
-const manifestUri =
-  "https://raw.githubusercontent.com/PowerShell/DSC/main/schemas/v3/resource/manifest.json";
-
-// The base schema is JSON 2020-12 hence the use of Ajv2020
-async function getManifestValidator(): Promise<ValidateFunction> {
-  // The loadSchema function will fetch everything referenced during compileAsync()
-  const ajv = new Ajv2020({ loadSchema: loadSchema, strict: false });
-  // URI formats have to be added manually to ajv
-  addFormats(ajv);
-  return ajv.compileAsync(await loadSchema(manifestUri));
 }
 
 async function main() {
@@ -36,7 +24,15 @@ async function main() {
     .option("-d, --debug", "Enable debug logging");
   await program.parseAsync(process.argv);
 
-  const validateManifest = await getManifestValidator();
+  // The base schema is JSON 2020-12 hence the use of Ajv2020
+  // The loadSchema function will fetch everything referenced during compileAsync()
+  const ajv = new Ajv2020({ loadSchema: loadSchema, strict: false });
+  // URI formats have to be added manually to ajv
+  addFormats(ajv);
+  const manifestSchema = await loadSchema(
+    "https://aka.ms/dsc/schemas/v3/bundled/resource/manifest.json"
+  );
+  const validateResource = await ajv.compileAsync(manifestSchema);
 
   // TODO: Iterate over all schemas not just this test one
   const uri =
@@ -44,7 +40,7 @@ async function main() {
   const schema = await loadSchema(uri);
   const dscType = schema.type as string;
   console.log(`DSC type: ${dscType}`);
-  validateManifest(schema);
+  validateResource(schema);
   // TODO: Uhh ok great I can validate the schema but how do I iterate over it?
 }
 
