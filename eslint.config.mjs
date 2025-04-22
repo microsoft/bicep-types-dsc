8b6364b168211bc0667ceacf971079c55fe95b0e
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-04-21 17:06:45 -0700

    Use Prettier via on-save not via ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import eslint from "@eslint/js";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
-  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       parserOptions: {
