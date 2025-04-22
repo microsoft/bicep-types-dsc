f5b70402971e438803fca0b2130eada52659988f
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-04-21 17:14:59 -0700

    Basic CLI so I can try this on schema files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+import { Command } from "commander";
+import { version } from "../package.json";
 
 function main() {
-  console.log("Hello, world!");
+  const program = new Command();
+  program
+    .version(version)
+    .description(
+      "A CLI tool for generating Bicep types from DSC resource schemas"
+    )
+    .option("-d, --debug", "Enable debug logging")
+    .argument("<file>", "The schema file to process");
+  program.parse(process.argv);
+
+  if (program.opts().debug) {
+    console.log("Debug logging enabled!");
+    console.log(program.args);
+  }
 }
 
 main();
