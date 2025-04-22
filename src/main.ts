caf3440f4f97d6f76d00084f68cb690a5d6b6377
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-04-21 20:29:48 -0700

    A debatably useful validator

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { version } from "../package.json";
 // TODO: import { TypeFactory } from "bicep-types";
 import axios from "axios";
 import addFormats from "ajv-formats";
-import Ajv2020 from "ajv/dist/2020";
+import Ajv2020, { type ValidateFunction } from "ajv/dist/2020";
 
 type UnknownJson = Record<string, unknown>;
 
@@ -14,20 +14,16 @@ async function loadSchema(uri: string): Promise<UnknownJson> {
   return response.data as UnknownJson;
 }
 
-async function getManifestValidator(): Promise<Ajv2020> {
-  const manifestUri =
-    "https://aka.ms/dsc/schemas/v3/bundled/resource/manifest.json";
-  const manifestSchema = await loadSchema(manifestUri);
-  // TODO: Our published schema has the old $id, so we need to override it
-  manifestSchema.$id = manifestUri;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-  (manifestSchema as any).properties.$schema.enum.push(
-    "https://aka.ms/dsc/schemas/v3/bundled/resource/manifest.json"
-  );
+const manifestUri =
+  "https://raw.githubusercontent.com/PowerShell/DSC/main/schemas/v3/resource/manifest.json";
+
+// The base schema is JSON 2020-12 hence the use of Ajv2020
+async function getManifestValidator(): Promise<ValidateFunction> {
+  // The loadSchema function will fetch everything referenced during compileAsync()
   const ajv = new Ajv2020({ loadSchema: loadSchema, strict: false });
+  // URI formats have to be added manually to ajv
   addFormats(ajv);
-  ajv.addSchema(manifestSchema, manifestUri, true);
-  return ajv;
+  return ajv.compileAsync(await loadSchema(manifestUri));
 }
 
 async function main() {
@@ -40,15 +36,16 @@ async function main() {
     .option("-d, --debug", "Enable debug logging");
   await program.parseAsync(process.argv);
 
-  const manifestValidator = await getManifestValidator();
-  if (program.opts().debug) console.log(manifestValidator.schemas);
+  const validateManifest = await getManifestValidator();
 
+  // TODO: Iterate over all schemas not just this test one
   const uri =
     "https://raw.githubusercontent.com/PowerShell/DSC/refs/heads/main/registry/registry.dsc.resource.json";
   const schema = await loadSchema(uri);
-  const validate = manifestValidator.addSchema(schema);
-
-  console.log(validate.errors);
+  const dscType = schema.type as string;
+  console.log(`DSC type: ${dscType}`);
+  validateManifest(schema);
+  // TODO: Uhh ok great I can validate the schema but how do I iterate over it?
 }
 
 await main();
