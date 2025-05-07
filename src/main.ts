f4f1cc097b9ed4b45bef0fd6baca6ba932bb5efe
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-05-07 15:48:26 -0700

    Use json-schema package
    This is what I was looking for!

--- src/main.ts
+++ src/main.ts
@@ -10,76 +10,97 @@ import {
   TypeReference,
 } from "bicep-types";
 import { Command } from "commander";
+import type { JSONSchema7, JSONSchema7Definition } from "json-schema";
 import { version } from "../package.json";
 
 interface ManifestSchema {
   type: string;
   version: string;
-  description: string;
   schema: {
-    embedded?: Schema;
+    embedded?: JSONSchema7;
     command?: unknown;
   };
 }
 
-interface Schema {
-  type: string;
-  title?: string;
-  description?: string;
-  maximum?: number;
-  minimum?: number;
-  maxLength?: number;
-  minLength?: number;
-  pattern?: string;
-  items?: Schema;
-  properties: Record<string, Schema>;
-}
+function createType(
+  factory: TypeFactory,
+  schema: JSONSchema7Definition,
+): TypeReference {
+  // TODO: What do?
+  if (typeof schema === "boolean") {
+    throw new Error(`Boolean definitions are not supported!`);
+  }
 
-function createType(factory: TypeFactory, schema: Schema): TypeReference {
+  if (schema.type === undefined) {
+    throw new Error(`Schema type is undefined!`);
+  }
+
+  // TODO: Handle array?
+  if (Array.isArray(schema.type)) {
+    throw new Error(`Array schema types are not supported!`);
+  }
+
   switch (schema.type) {
     case "null": {
       return factory.addNullType();
     }
+
     case "boolean": {
       return factory.addBooleanType();
     }
+
     case "integer":
     case "number": {
       return factory.addIntegerType(schema.minimum, schema.maximum);
     }
+
     case "string": {
       return factory.addStringType(
-        true,
+        true, // sensitive
         schema.minLength,
         schema.maxLength,
         schema.pattern,
       );
     }
+
     case "array": {
-      if (!schema.items) {
+      if (schema.items === undefined) {
         throw new Error(`Array type missing items definition: ${schema.type}`);
       }
+
+      // TODO: Handle boolean and array?
+      if (typeof schema.items === "boolean" || Array.isArray(schema.items)) {
+        throw new Error(`Unsupported items type in array type: ${schema.type}`);
+      }
+
       return factory.addArrayType(
         createType(factory, schema.items),
         schema.minLength,
         schema.maxLength,
       );
     }
+
     case "object": {
+      if (schema.properties === undefined) {
+        throw new Error(
+          `Object type missing properties definition: ${schema.type}`,
+        );
+      }
+
+      // Is this seriously the only way to map one Record into another in TypeScript?!
       const properties: Record<string, ObjectTypeProperty> = Object.fromEntries(
         Object.entries(schema.properties).map(([key, value]) => [
           key,
           {
             type: createType(factory, value),
             flags: ObjectTypePropertyFlags.None,
-            description: value.description,
+            description:
+              typeof value !== "boolean" ? value.description : undefined,
           },
         ]),
       );
+
       return factory.addObjectType(schema.title ?? "object", properties);
-    }
-    default: {
-      throw new Error(`Unsupported schema type: ${schema.type}`);
     }
   }
 }
@@ -101,11 +122,13 @@ async function main(): Promise<number> {
   const resource = response.data as ManifestSchema;
 
   if (resource.schema.command) {
-    console.error("TODO: Handle commands schema!");
+    // TODO: Run `dsc schema` command and extract the generated schema to parse.
+    console.error("Command-based schema not yet supported!");
     return 1;
   }
 
-  if (!resource.schema.embedded) {
+  if (resource.schema.embedded === undefined) {
+    // TODO: Right now this is all that's supported and therefore required.
     console.error("Embedded schema is not defined!");
     return 1;
   }
@@ -124,4 +147,5 @@ async function main(): Promise<number> {
   return 0;
 }
 
+// I'm still just a C programmer.
 process.exit(await main());
